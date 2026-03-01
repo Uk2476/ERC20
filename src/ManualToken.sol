052505868fec0ee0c4804a8f8aabba46c5f2d12d
Uk2476 <kuttkarsh028@gmail.com>
2026-03-01 05:38:19 +0530

    ERROR FIXED

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -1,13 +1,14 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
+error invalidtransaction();
 error insufficientBalance();
 
 contract ManualToken{
 
     mapping (address=>uint256) accountBalance  ;
 
-    string public constant name = "Manual token";
+    string public constant NAME = "Manual token";
 
     function totalSupply() public pure returns(uint256){
         return 100 ether ;
@@ -17,12 +18,12 @@ contract ManualToken{
         return 18;
     }
 
-    function balance(address memory _accountAddress) public view returns(uint256){
-        return accountBalance[accountAddress];
+    function balance(address _accountAddress) public view returns(uint256){
+        return accountBalance[_accountAddress];
     }
 
-    function transfer(address memory _reciever , uint256 _amountToSend) public {
-        uint256 memory totalBalanceofTheseTwo = balance(_reciever)+balance(msg.sender);
+    function transfer(address  _reciever , uint256 _amountToSend) public {
+        uint256  totalBalanceofTheseTwo = balance(_reciever)+balance(msg.sender);
         require(accountBalance[msg.sender]>_amountToSend , insufficientBalance());
         accountBalance[msg.sender]-=_amountToSend;
         accountBalance[_reciever]+=_amountToSend;
